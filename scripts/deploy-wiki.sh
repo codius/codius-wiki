709000adbaf515120d2ded3d0619fca05e612c14
njlie <lie4nathan@gmail.com>
2018-06-12 14:20:16 -0700

    chore: log directory locations

--- scripts/deploy-wiki.sh
+++ scripts/deploy-wiki.sh
@@ -8,7 +8,7 @@ set -e
 
 # show where we are on the machine
 pwd
-
+ls
 # make a directory to put the gp-pages branch
 mkdir codius-wiki-branch
 cd codius-wiki-branch || exit
@@ -31,6 +31,8 @@ else
 fi
 
 # copy over or recompile the new site
+pwd
+ls
 cp -a "../codius-wiki/." .
 
 # stage any changes and new files
